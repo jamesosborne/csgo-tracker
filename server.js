e93085aa571a37d2718e87ce8d358b9b8d5f9adc
James Osborne <jameseosborne@outlook.com>
2022-06-20 13:38:24 +0100

    server setup and env variables

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+
+//Load env
+dotenv.config({path: './config.env'});
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.listen(port, ()=>{
+    console.log(`Server listening in ${process.env.NODE_ENV} on port ${port}`);
+});
