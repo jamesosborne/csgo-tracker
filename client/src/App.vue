5d006f69bf10904a30106285422b08e1a93d7607
James Osborne <jameseosborne@outlook.com>
2022-06-21 23:04:31 +0100

    added routes and api fetch in profile

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="container">
     <Header />
-    <Search />
+    <router-view />
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
-import Search from './components/Search.vue'
 export default {
   name: "app",
   components: {
-    Header,
-    Search
+    Header
   }
 }
 </script>
--- client/vue.config.js
+++ client/vue.config.js
@@ -1,5 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave: false
+  lintOnSave: false,
+  devServer: {
+    proxy: {
+      '/api/v1': {
+        target: 'http://localhost:5000'
+      }
+    }
+  }
 })
