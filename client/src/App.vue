9320c3c04cd793b95f6f2274428115cdf9024603
James Osborne <jameseosborne@outlook.com>
2022-06-21 18:28:52 +0100

    created header component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,14 +1,15 @@
 <template>
-  <div class="container">Hello world!</div>
+  <div class="container">
+    <Header />
+  </div>
 </template>
 
 <script>
-
-
+import Header from './components/Header.vue'
 export default {
   name: 'App',
   components: {
-
+    Header
   }
 }
 </script>
