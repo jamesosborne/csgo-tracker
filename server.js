92ce7a71b679693cf55595ce7f87eb5bb948ddae
James Osborne <jameseosborne@outlook.com>
2022-06-22 08:10:43 +0100

    built application and fixed error msg log

--- server.js
+++ server.js
@@ -16,6 +16,15 @@ if(process.env.NODE_ENV === 'development'){
 //Profile roues
 app.use('/api/v1/profile', require('./routes/profile'))
 
+//Handle production
+if(process.env.NODE_ENV === 'production'){
+    //Set static folder
+    app.use(express.static(__dirname + '/public/'))
+
+    //Handle SPA
+    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'))
+}
+
 app.listen(port, ()=>{
     console.log(`Server listening in ${process.env.NODE_ENV} on port ${port}`);
 }); 
